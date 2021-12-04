2601b17d9b2dc2ab18756b2dbdc919229544df93
Julius Makowski <jogius.julius.makowski@gmail.com>
2021-12-04 13:02:28 +0100

    feat: argv for device_id and sensor_type

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 > A small python script that fetches data from MyDevolo Smart Home devices
 
 ## Running the script
-Copy the ``.env.example`` file to ``.env`` and add the values associated with the keys (MyDevolo username, password, the device's name and the sensor type you would like to read, e.g. ``temperature`` or ``light``).
+Copy the ``.env.example`` file to ``.env`` and add the values associated with the keys (MyDevolo username, password).
+Then run the script (``python3 src/main.py <device_id> <sensor_type>``), where ``device_id`` is in the format hdm:ZWave:XXXXXXXX/XX and ``sensor_type`` can be e.g. ``temperature`` or ``light``.
--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+from sys import argv
+
+if len(argv) < 3:
+  exit(1)
+
+device_id = argv[1]
+sensor_type = argv[2]
+
 import logging
 import os
 from dotenv import load_dotenv
@@ -16,8 +24,12 @@ mydevolo.password = os.getenv("DEVOLO_PASSWORD")
 gateway_id = mydevolo.get_gateway_ids()[0]
 with HomeControl(gateway_id=gateway_id, mydevolo_instance=mydevolo) as homecontrol :
 
-  device = homecontrol.devices.get(homecontrol.device_names.get(os.getenv("DEVOLO_DEVICE_NAME")))
+  if device_id not in homecontrol.devices:
+    exit(1)
+
+  device = homecontrol.devices.get(device_id)
+  
   sensor = device.get_property("multi_level_sensor")
   for s in sensor:
-    if s.sensor_type == os.getenv("DEVOLO_SENSOR_TYPE"):
+    if s.sensor_type == sensor_type:
       print(s.value)
